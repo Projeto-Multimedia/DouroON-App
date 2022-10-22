e2cda3f286874b816687edbe1b4bcb3672237451
Simão Amaral <simao.ramaral@gmail.com>
2022-10-22 22:47:41 +0100

    Preliminary welcome screen with Nativewind

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,70 +1,40 @@
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
-import React from 'react';
-import { useNavigation } from '@react-navigation/native';
-import { UserList } from '../components/UserList';
+import { Text, View, Dimensions, TouchableOpacity, ImageBackground, Image } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { UserList } from "../components/UserList";
 
-const SCREENHEIGHT = Dimensions.get('window').height;
-const SCREENWIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 const WelcomeScreen = () => {
-    const navigation = useNavigation();
+	const navigation = useNavigation();
 
-    return ( 
-        <View>
-            <UserList />
-            <View style={styles.textContainer}>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Signup')}>
-                <Text style={[styles.text, {
-                    fontSize: 18,
-                    color: 'black',
-                    top: 0,
-                    alignSelf: 'center'
-                }]}>
-                    Get started
+	return (
+		<View className="container mx-auto">
+			<View className="space-y-6 p-6">
+				<Text className="text-neutral-900 font-semibold text-2xl">Bem-vindo 👋</Text>
+				<Text className="text-base text-neutral-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+				<TouchableOpacity
+					className="bg-neutral-500 rounded-2xl px-8 py-4"
+					onPress={() => navigation.navigate("Signup")}
+				>
+					<Text className="text-neutral-50 text-center text-2xl">Vamos começar!</Text>
+				</TouchableOpacity>
+			</View>
+			<View className="flex flex-row justify-between mx-auto">
+				<Text>-- </Text>
+				<Text className="text-neutral-400 text-sm">OU</Text>
+				<Text> --</Text>
+			</View>
+            <View className="mx-auto mt-6">
+                <Text className="text-neutral-400">Já tem conta? 
+                    <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
+                        <Text className="text-neutral-500"> Faça login</Text>
+                    </TouchableOpacity>
                 </Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-                <Text style={[styles.text, {
-                    fontSize: 16,
-                    color: 'white',
-                    top: 90,
-                    alignSelf: 'center'
-                }]}>
-                    Log in
-                </Text>
-            </TouchableOpacity>
-        </View>
-    </View>
-    );
+            </View>
+		</View>
+	);
+};
 
-}
-
-export default WelcomeScreen
-
-const styles = StyleSheet.create({
-    button: {
-        backgroundColor: 'white',
-        width: '80%',
-        height: 55,
-        alignItems: 'center',
-        justifyContent: 'center',
-        alignSelf: 'center',
-        borderRadius: 15,
-        top: 70,
-    },
-    text: {
-        fontFamily: 'Montserrat',
-        fontSize: 38,
-        color: 'white',
-        alignSelf: 'center',
-        top: 20
-    },
-    textContainer: {
-        height: SCREENHEIGHT/2.8,
-        width: SCREENWIDTH,
-        backgroundColor: 'black',
-        top: SCREENHEIGHT-(SCREENHEIGHT/2.8),
-        borderTopLeftRadius: 15,
-        borderTopRightRadius: 15,
-    },
-});
+export default WelcomeScreen;
