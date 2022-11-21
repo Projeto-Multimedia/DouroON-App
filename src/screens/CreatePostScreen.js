f5fd8bfa5dc7f419d0c3ff22dbfdd5f746607367
Simão Amaral <simao.ramaral@gmail.com>
2022-11-21 16:55:07 +0000

    company post (bugged)

--- src/screens/CreatePostScreen.js
+++ src/screens/CreatePostScreen.js
@@ -1,11 +1,17 @@
-import { Text, View, TouchableOpacity, Image, TextInput, ScrollView } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import React, { useContext, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { AuthContext } from "../context/AuthContext";
-import * as ImagePicker from 'expo-image-picker';
+import * as ImagePicker from "expo-image-picker";
 
 const CreatePostScreen = () => {
-  const navigation = useNavigation();
   const { endUserInfo } = useContext(AuthContext);
 
   const [post, setPost] = useState({
@@ -15,10 +21,9 @@ const CreatePostScreen = () => {
     description: "",
   });
 
-  const [image, setImage] = useState(null);
-
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
+    console.log(endUserInfo.profile);
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
@@ -27,45 +32,60 @@ const CreatePostScreen = () => {
     });
 
     if (!result.canceled) {
-      setImage(result.assets[0].uri);
+      let fileType = result.assets[0].uri.substring(
+        result.assets[0].uri.lastIndexOf(".") + 1,
+      );
+      let fileName = result.assets[0].uri.substring(
+        result.assets[0].uri.lastIndexOf("/") + 1,
+      );
+
+      const data = new FormData();
+      data.append("image", {
+        uri: result.assets[0].uri,
+        type: `image/${fileType}`,
+        name: fileName,
+      });
+
+      setPost({ ...post, image: data });
     }
   };
 
-  function _createPost() {
-    console.log(post);
-  }
+  const _createPost = async (image) => {
+    const data = new FormData();
+    data.append("location", post.location);
+    data.append("description", post.description);
 
-  const uploadImage = async (image) => {
-    await fetch(`http://10.0.2.2:8000/api/user-posts/${endUserInfo.id}/create`, {
-      method: "POST",
-      headers: {
-        "content-type": "multipart/form-data",
+    await fetch(
+      `http://10.0.2.2:8000/api/${endUserInfo.profile}-posts/${endUserInfo.id}/create`,
+      {
+        method: "POST",
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+        body: image,
       },
-      body: image,
-    })
+    )
       .then((res) => res.json())
       .then((res) => {
         console.log("Success:", res.status + " - " + res.message);
-        setEndUserInfo(res.data);
-        AsyncStorage.setItem("endUserInfo", JSON.stringify(res.data));
+        let arr = res.data;
+        setPost({ arr });
       })
       .catch((err) => console.log("err: ", err));
-  };
+    };
 
   return (
     <ScrollView className="container mt-8 p-5 flex-1 bg-neutral-900">
-        <View className="flex flex-row justify-between items-center">
-            <Text className="text-neutral-50 font-semibold text-2xl">
-                New post
-            </Text>
-        </View>
-        <Image
+      <View className="flex flex-row justify-between items-center">
+        <Text className="text-neutral-50 font-semibold text-2xl">New post</Text>
+      </View>
+      <Image
         className="mx-auto my-4"
         source={{
-          uri: `http://10.0.2.2:8000/${endUserInfo.avatar}`,
+          uri: `http://10.0.2.2:8000/${post.image}`,
         }}
-        key={`http://10.0.2.2:8000/${endUserInfo.avatar}`}
-        style={{ width: '100%', height: undefined, aspectRatio: 1 }}
+        key={`http://10.0.2.2:8000/${post.image}`}
+        style={{ width: "100%", height: undefined, aspectRatio: 1 }}
         resizeMode="cover"
       ></Image>
       <View className="flex flex-row justify-between items-center">
@@ -76,15 +96,15 @@ const CreatePostScreen = () => {
           autoCapitalize="none"
           onChangeText={(location) => setPost({ ...post, location })}
         ></TextInput>
-        </View>
-        <TextInput
-          className="mt-4 bg-neutral-900 border border-neutral-400 text-neutral-100 px-3 py-2 rounded-lg"
-          placeholder={"Write a caption..."}
-          placeholderTextColor={"#A3A3A3"}
-          autoCapitalize="none"
-          onChangeText={(description) => setPost({ ...post, description })}
-        ></TextInput>
-        <TouchableOpacity
+      </View>
+      <TextInput
+        className="mt-4 bg-neutral-900 border border-neutral-400 text-neutral-100 px-3 py-2 rounded-lg"
+        placeholder={"Write a caption..."}
+        placeholderTextColor={"#A3A3A3"}
+        autoCapitalize="none"
+        onChangeText={(description) => setPost({ ...post, description })}
+      ></TextInput>
+      <TouchableOpacity
         className="bg-emerald-500 rounded-lg px-2 py-2 mt-6 mx-20"
         onPress={() => pickImage()}
       >
@@ -94,7 +114,7 @@ const CreatePostScreen = () => {
       </TouchableOpacity>
       <TouchableOpacity
         className="bg-emerald-500 rounded-lg px-2 py-2 mt-6 mx-20"
-        onPress={() => _createPost()}
+        onPress={() => _createPost(post.image)}
       >
         <Text className="font-medium text-neutral-50 text-center text-xl">
           Create post
