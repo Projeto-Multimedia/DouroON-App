dc564e2f478d11940750fd54c0bf80544d241944
Simão Amaral <simao.ramaral@gmail.com>
2022-11-21 18:24:07 +0000

    Image fixes

--- src/screens/CreatePostScreen.js
+++ src/screens/CreatePostScreen.js
@@ -5,11 +5,13 @@ import {
   Image,
   TextInput,
   ScrollView,
+  SafeAreaView,
 } from "react-native";
 import React, { useContext, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { AuthContext } from "../context/AuthContext";
 import * as ImagePicker from "expo-image-picker";
+import { Button } from "../components/Button";
 
 const CreatePostScreen = () => {
   const { endUserInfo } = useContext(AuthContext);
@@ -20,6 +22,8 @@ const CreatePostScreen = () => {
     location: "",
     description: "",
   });
+
+  const [url, setUrl] = useState("");
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -47,13 +51,21 @@ const CreatePostScreen = () => {
       });
 
       setPost({ ...post, image: data });
+      setUrl(result.assets[0].uri);
     }
   };
 
   const _createPost = async (image) => {
-    const data = new FormData();
-    data.append("location", post.location);
-    data.append("description", post.description);
+    const data = image;
+
+    if (post.location) {
+      data.append("location", post.location);
+    }
+    else if (post.description) {
+      data.append("description", post.description);
+    }
+      
+    console.log(data);
 
     await fetch(
       `http://10.0.2.2:8000/api/${endUserInfo.profile}-posts/${endUserInfo.id}/create`,
@@ -62,7 +74,7 @@ const CreatePostScreen = () => {
         headers: {
           "content-type": "multipart/form-data",
         },
-        body: image,
+        body: data,
       },
     )
       .then((res) => res.json())
@@ -75,53 +87,41 @@ const CreatePostScreen = () => {
     };
 
   return (
-    <ScrollView className="container mt-8 p-5 flex-1 bg-neutral-900">
+    <SafeAreaView className="bg-neutral-900 flex-1">
+    <ScrollView className="mt-8 p-5">
       <View className="flex flex-row justify-between items-center">
         <Text className="text-neutral-50 font-semibold text-2xl">New post</Text>
       </View>
+      {url ? 
       <Image
         className="mx-auto my-4"
-        source={{
-          uri: `http://10.0.2.2:8000/${post.image}`,
-        }}
-        key={`http://10.0.2.2:8000/${post.image}`}
+        source={{ uri: url }}
         style={{ width: "100%", height: undefined, aspectRatio: 1 }}
         resizeMode="cover"
       ></Image>
-      <View className="flex flex-row justify-between items-center">
+      : null}
+      <View>
         <TextInput
-          className="bg-neutral-900 border border-neutral-400 text-neutral-100 px-3 py-2 rounded-lg"
+          className="mt-4 bg-neutral-900 border border-neutral-400 text-neutral-100 px-3 py-2 rounded-lg"
           placeholder={"Location"}
           placeholderTextColor={"#A3A3A3"}
           autoCapitalize="none"
-          onChangeText={(location) => setPost({ ...post, location })}
+          value={post.location}
+          onChangeText={(location) => setPost({ ...post, location: location })}
         ></TextInput>
-      </View>
-      <TextInput
+        <TextInput
         className="mt-4 bg-neutral-900 border border-neutral-400 text-neutral-100 px-3 py-2 rounded-lg"
         placeholder={"Write a caption..."}
         placeholderTextColor={"#A3A3A3"}
         autoCapitalize="none"
-        onChangeText={(description) => setPost({ ...post, description })}
+        value={post.description}
+        onChangeText={(description) => setPost({ ...post, description: description })}
       ></TextInput>
-      <TouchableOpacity
-        className="bg-emerald-500 rounded-lg px-2 py-2 mt-6 mx-20"
-        onPress={() => pickImage()}
-      >
-        <Text className="font-medium text-neutral-50 text-center text-xl">
-          Upload image
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        className="bg-emerald-500 rounded-lg px-2 py-2 mt-6 mx-20"
-        onPress={() => _createPost(post.image)}
-      >
-        <Text className="font-medium text-neutral-50 text-center text-xl">
-          Create post
-        </Text>
-      </TouchableOpacity>
-      <View className="mt-4 space-y-4"></View>
+      </View>
+        <Button message="Upload image" textColor="text-neutral-900" color="bg-white" onPress={() => pickImage()}/>
+        <Button message="Create post" textWeight="font-medium" textColor="text-neutral-50" color="bg-emerald-500" onPress={() => _createPost(post.image)}/>
     </ScrollView>
+    </SafeAreaView>
   );
 };
 
