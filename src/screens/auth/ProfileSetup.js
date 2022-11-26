544a4d11d3abdce6426d827547d0dbe84122ee91
Cascavel <simao.ramaral@gmail.com>
2022-11-26 17:43:23 +0000

    Updated register screens to scroll views so they don't hide the inputs

--- src/screens/auth/ProfileSetup.js
+++ src/screens/auth/ProfileSetup.js
@@ -1,4 +1,12 @@
-import { TextInput, Text, View, TouchableOpacity, Image } from "react-native";
+import {
+  TextInput,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
 import React, { useContext, useState } from "react";
 import { useRoute, useNavigation } from "@react-navigation/native";
 
@@ -25,52 +33,57 @@ export const ProfileSetup = () => {
       let arr = res;
       setAlertMessage(arr.message);
       setEndUser(arr);
-      
+
       if (arr.status === "success") {
         navigation.navigate("SignIn");
       }
-    })
+    });
   }
 
   return (
-    <View className="mt-8 p-5">
-      <Text className="text-neutral-50 font-semibold text-3xl">Profile</Text>
-      <Image
-        className="mx-auto my-4"
-        source={require("../../assets/avatar_default.png")}
-      ></Image>
-      <TouchableOpacity className="bg-neutral-50 rounded-lg px-4 py-2">
-        <Text className="text-neutral-900 text-center text-xl">
-          Add a profile picture
-        </Text>
-      </TouchableOpacity>
-      <View className="mt-4 space-y-4">
-      <Alert class="bg-red-100 font-semibold text-red-900 px-4 py-3 rounded" alert={alertMessage} />
-        <Text className="text-xl text-neutral-50">Full name</Text>
-        <TextInput
-          className="bg-neutral-900 border border-neutral-400 text-neutral-400 px-3 py-2 rounded-lg"
-          placeholder="ex: John Doe"
-          placeholderTextColor={"#A3A3A3"}
-          autoCapitalize="none"
-          value={end_user.name}
-          onChangeText={(name) => setEndUser({ ...end_user, name })}
-        ></TextInput>
-        <Text className="text-xl text-neutral-50">Username</Text>
-        <TextInput
-          className="bg-neutral-900 border border-neutral-400 text-neutral-400 px-3 py-2 rounded-lg"
-          placeholder="Your username"
-          placeholderTextColor={"#A3A3A3"}
-          value={end_user.username}
-          onChangeText={(username) => setEndUser({ ...end_user, username })}
-        ></TextInput>
-        <TouchableOpacity
-          className="bg-neutral-50 rounded-lg px-4 py-2"
-          onPress={() => _createEndUser(end_user)}
-        >
-          <Text className="text-neutral-900 text-center text-xl">Finish</Text>
+    <SafeAreaView>
+      <ScrollView className="mt-8 p-5">
+        <Text className="text-neutral-50 font-semibold text-3xl">Profile</Text>
+        <Image
+          className="mx-auto my-4"
+          source={require("../../assets/avatar_default.png")}
+        ></Image>
+        <TouchableOpacity className="bg-neutral-50 rounded-lg px-4 py-2">
+          <Text className="text-neutral-900 text-center text-xl">
+            Add a profile picture
+          </Text>
         </TouchableOpacity>
-      </View>
-    </View>
+        <View className="mt-4 space-y-4">
+          <Alert
+            class="bg-red-100 font-semibold text-red-900 px-4 py-3 rounded"
+            alert={alertMessage}
+          />
+          <Text className="text-xl text-neutral-50">Full name</Text>
+          <TextInput
+            className="bg-neutral-900 border border-neutral-400 text-neutral-50 px-3 py-2 rounded-lg"
+            placeholder="ex: John Doe"
+            placeholderTextColor={"#A3A3A3"}
+            autoCapitalize="none"
+            value={end_user.name}
+            onChangeText={(name) => setEndUser({ ...end_user, name })}
+          ></TextInput>
+          <Text className="text-xl text-neutral-50">Username</Text>
+          <TextInput
+            className="bg-neutral-900 border border-neutral-400 text-neutral-50 px-3 py-2 rounded-lg"
+            placeholder="Your username"
+            placeholderTextColor={"#A3A3A3"}
+            value={end_user.username}
+            onChangeText={(username) => setEndUser({ ...end_user, username })}
+          ></TextInput>
+          <TouchableOpacity
+            className="bg-neutral-50 rounded-lg px-4 py-2"
+            onPress={() => _createEndUser(end_user)}
+          >
+            <Text className="text-neutral-900 text-center text-xl">Finish</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
--- src/screens/auth/Signup.js
+++ src/screens/auth/Signup.js
@@ -1,48 +1,64 @@
-import { TextInput, Text, View, TouchableOpacity } from 'react-native';
-import React, { useState} from 'react';
-import { useNavigation } from '@react-navigation/native';
+import {
+  TextInput,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const Signup = () => {
   const navigation = useNavigation();
 
   const [end_user, setEndUser] = useState({
-    username: '',
-    name: '',
-    email: '',
-    password: '',
+    username: "",
+    name: "",
+    email: "",
+    password: "",
   });
 
   return (
-      <View className="mt-8 p-5">
-          <Text className="text-neutral-50 font-semibold text-3xl">Join DouroON</Text>
-          <View className="mt-4 space-y-4">
-            <Text className="text-xl text-neutral-50">Email</Text>
-            <TextInput
-              className="bg-neutral-900 border border-neutral-400 text-neutral-50 px-3 py-2 rounded-lg"
-              placeholder="Your e-mail"
-              placeholderTextColor={"#A3A3A3"}
-              autoCapitalize="none"
-              value={end_user.email}
-              onChangeText={(email) => setEndUser({ ...end_user, email })}
-            ></TextInput>
-            <Text className="text-xl text-neutral-50">Password</Text>
-            <TextInput
-              className="bg-neutral-900 border border-neutral-400 text-neutral-50 px-3 py-2 rounded-lg"
-              placeholder="Your password"
-              placeholderTextColor={"#A3A3A3"}
-              secureTextEntry={true}
-              value={end_user.password}
-              onChangeText={(password) => setEndUser({ ...end_user, password })}
-            ></TextInput>
-            <TouchableOpacity
-              className="bg-neutral-50 rounded-lg px-4 py-2"
-              onPress={() => navigation.navigate("ProfileSetup", { email: end_user.email, password: end_user.password })}
-            >
-              <Text className="text-neutral-900 text-center text-xl">Next</Text>
-            </TouchableOpacity>
-          </View>
-      </View>
-    );
-}
+    <SafeAreaView>
+      <ScrollView className="mt-8 p-5">
+        <Text className="text-neutral-50 font-semibold text-3xl">
+          Join DouroON
+        </Text>
+        <View className="mt-4 space-y-4">
+          <Text className="text-xl text-neutral-50">Email</Text>
+          <TextInput
+            className="bg-neutral-900 border border-neutral-400 text-neutral-50 px-3 py-2 rounded-lg"
+            placeholder="Your e-mail"
+            placeholderTextColor={"#A3A3A3"}
+            autoCapitalize="none"
+            value={end_user.email}
+            onChangeText={(email) => setEndUser({ ...end_user, email })}
+          ></TextInput>
+          <Text className="text-xl text-neutral-50">Password</Text>
+          <TextInput
+            className="bg-neutral-900 border border-neutral-400 text-neutral-50 px-3 py-2 rounded-lg"
+            placeholder="Your password"
+            placeholderTextColor={"#A3A3A3"}
+            secureTextEntry={true}
+            value={end_user.password}
+            onChangeText={(password) => setEndUser({ ...end_user, password })}
+          ></TextInput>
+          <TouchableOpacity
+            className="bg-neutral-50 rounded-lg px-4 py-2"
+            onPress={() =>
+              navigation.navigate("ProfileSetup", {
+                email: end_user.email,
+                password: end_user.password,
+              })
+            }
+          >
+            <Text className="text-neutral-900 text-center text-xl">Next</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Signup;
