c4a7e66564728f074c5126cc2f00dfe2400b3ba6
Cascavel <simao.ramaral@gmail.com>
2022-11-28 13:45:51 +0000

    FlatList buttons fixed

--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -1,8 +1,15 @@
 import React, { useContext, useState, useEffect, useRef } from "react";
-import { View, FlatList, Text, Dimensions } from "react-native";
+import {
+  View,
+  FlatList,
+  Text,
+  Dimensions,
+  ActivityIndicator,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import { AuthContext } from "../context/AuthContext";
 
-import { FeedButtons } from "../components/FeedButtons";
 import PostSingle from "../components/Post";
 
 import apiUserPosts from "../services/api/posts_api";
@@ -14,8 +21,8 @@ export const FeedScreen = () => {
   const mediaRefs = useRef([]);
 
   const loadPosts = () => {
-    apiUserPosts.getAll().then((res) => {
-      setPosts(res);
+    apiUserPosts.getSingle(`${endUserInfo.id}/follows`).then((res) => {
+      setPosts(res.data);
     });
   };
 
@@ -23,31 +30,47 @@ export const FeedScreen = () => {
 
   const renderItem = ({ item, index }) => {
     return (
-      <View
-        style={[
-          { flex: 1, height: Dimensions.get("window").height - 54},
-        ]}
-      >
-        <PostSingle item={item} ref={PostSingleRef => (mediaRefs.current[item.id] = PostSingleRef)}/>
+      <View style={[{ flex: 1, height: Dimensions.get("window").height - 54 }]}>
+        <PostSingle
+          item={item}
+          ref={(PostSingleRef) => (mediaRefs.current[item.id] = PostSingleRef)}
+        />
       </View>
     );
   };
   return (
-    <View>
-      <FeedButtons />
-      <FlatList
-        onRefresh={loadPosts}
-        refreshing={false}
-        data={posts}
-        windowSize={8}
-        initialNumToRender={3}
-        renderItem={renderItem}
-        pagingEnabled
-        keyExtractor={item => item.id}
-        decelerationRate={"normal"}
-      />
-    </View>
+    <SafeAreaView className="bg-neutral-900 flex-1">
+      {posts ? (
+        <>
+          <View className="flex flex-row justify-between px-11 absolute top-6 left-0 right-0 z-10">
+            <TouchableOpacity>
+              <Text className="font-semibold text-2xl text-center text-neutral-50">
+                Following
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text className="font-semibold text-2xl text-center text-neutral-50">
+                For You
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <FlatList
+            onRefresh={loadPosts}
+            refreshing={false}
+            data={posts}
+            windowSize={8}
+            initialNumToRender={3}
+            renderItem={renderItem}
+            pagingEnabled
+            keyExtractor={(item) => item.id}
+            decelerationRate={"normal"}
+          ></FlatList>
+        </>
+      ) : (
+        <ActivityIndicator className="flex-1 justify-center" size="large" />
+      )}
+    </SafeAreaView>
   );
-  };
+};
 
 export default FeedScreen;
