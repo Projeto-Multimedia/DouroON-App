a542e98d9c9e4f3c272f3aaa1cdb1367a5de312e
Cascavel <simao.ramaral@gmail.com>
2022-12-29 21:39:12 +0000

    Route screen update

--- src/screens/RoutesScreen.js
+++ src/screens/RoutesScreen.js
@@ -1,23 +1,49 @@
 import {
   Text,
-  ScrollView,
   View,
-  TouchableOpacity,
-  Image,
+  ScrollView,
   RefreshControl,
   SafeAreaView,
+  FlatList,
   ActivityIndicator,
 } from "react-native";
 import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+import apiUserRoutes from "../services/api/user_routes_api";
 
 export const RoutesScreen = () => {
+  const [allUserRoutes, setAllUserRoutes] = useState([]);
+
+  const { endUserInfo } = useContext(AuthContext);
+
+  const getAllUserRoutes = () => {
+    apiUserRoutes.getSingle(`${endUserInfo.profile_id}/routes`).then((res) => {
+      let arr = res;
+      setAllUserRoutes(arr.data);
+    });
+  };
+
+  useEffect(() => {
+    getAllUserRoutes();
+  }, []);
+
   return (
-    <SafeAreaView>
-      <ScrollView>
-        <View>
-          <Text>RoutesScreen</Text>
-        </View>
-      </ScrollView>
+    <SafeAreaView className="p-5 flex-1 bg-neutral-900">
+      <FlatList
+        data={allUserRoutes}
+        windowSize={8}
+        renderItem={({ item }) => (
+          <View className="flex flex-row items-center justify-between mt-4">
+            <View className="flex flex-row items-center">
+              <Text className="text-neutral-50">{item.route_name}</Text>
+            </View>
+          </View>
+        )}
+        initialNumToRender={3}
+        pagingEnabled
+        decelerationRate={"normal"}
+      ></FlatList>
     </SafeAreaView>
   );
 };
